2f5695d1b8eefe539eef4fa9fc7adfce79b06916
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-06-19 22:32:54 +0900

    サーバのチャット部分のUIパネルを作成した。 オブザーバパターンを利用する。

---
+++ src/main/java/gmi/boardgame/chat/ChatServerPanel.java
@@ -0,0 +1,13 @@
+package gmi.boardgame.chat;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class ChatServerPanel extends JPanel implements Observer {
+  @Override
+  public void update(Observable o, Object arg) {
+  }
+}
