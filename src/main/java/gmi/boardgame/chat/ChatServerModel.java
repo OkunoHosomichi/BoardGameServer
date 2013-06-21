a1151b37a5765019f304fb072df9585f889d01ce
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-06-21 20:57:42 +0900

    テストが通るようにgetClientList()とjoinClient()を実装した。

--- src/main/java/gmi/boardgame/chat/ChatServerModel.java
+++ src/main/java/gmi/boardgame/chat/ChatServerModel.java
@@ -1,19 +1,31 @@
 package gmi.boardgame.chat;
 
+import gmi.utils.exceptions.NullArgumentException;
+
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Observable;
 
 public class ChatServerModel extends Observable implements ChatModel {
+  private final List<Client> fClientList = new LinkedList<>();
 
   @Override
   public List<String> getClientList() {
-    // TODO 自動生成されたメソッド・スタブ
-    return null;
+    List<String> result = new LinkedList<>();
+    for (Client i : fClientList) {
+      result.add(i.getNickName());
+    }
+
+    return result;
   }
 
   @Override
   public void joinClient(Client client) {
-    // TODO 自動生成されたメソッド・スタブ
+    if (client == null) throw new NullArgumentException("client");
 
+    fClientList.add(client);
+
+    setChanged();
+    notifyObservers("clientList");
   }
 }
