9b7aa09fed2a8fc3458037bf6584ed33540fbe16
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-06-23 07:41:48 +0900

    ChatServerModel#getClientList()が返すリストを変更不能にした。 コメントを追加した。 コメントを修正した。

--- src/main/java/gmi/boardgame/chat/ChatServerModel.java
+++ src/main/java/gmi/boardgame/chat/ChatServerModel.java
@@ -3,22 +3,37 @@ package gmi.boardgame.chat;
 import gmi.utils.exceptions.NullArgumentException;
 import io.netty.channel.group.ChannelGroup;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Observable;
 
 public final class ChatServerModel extends Observable implements ChatModel {
+  /**
+   * 行の区切り文字。
+   */
   private static final String LINE_SEPARATOR = System.getProperty("line.separator");
   /**
    * チャットに参加しているクライアントの一覧。
    */
   private final List<Client> fClientList = new LinkedList<>();
   /**
+   * チャンネルのグループ。
+   */
+  private final ChannelGroup fGroup;
+  /**
    * ユーザに通知するメッセージ。
    */
   private final StringBuilder fMessage = new StringBuilder();
-  private final ChannelGroup fGroup;
 
+  /**
+   * 指定されたチャンネルグループからインスタンスを構築します。
+   * 
+   * @param group
+   *          チャンネルグループ。nullを指定できません。
+   * @throws NullPointerException
+   *           groupがnullの場合。
+   */
   public ChatServerModel(ChannelGroup group) {
     fGroup = group;
   }
@@ -30,7 +45,7 @@ public final class ChatServerModel extends Observable implements ChatModel {
       result.add(i.getNickName());
     }
 
-    return result;
+    return Collections.unmodifiableList(result);
   }
 
   @Override
