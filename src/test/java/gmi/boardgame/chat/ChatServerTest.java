d35c55f77a59be062d36baef1d3f24426501a829
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-06-24 18:37:01 +0900

    ChatServerのコンストラクタのテストを作成した。

--- src/test/java/gmi/boardgame/chat/ChatServerTest.java
+++ src/test/java/gmi/boardgame/chat/ChatServerTest.java
@@ -1,4 +1,28 @@
 package gmi.boardgame.chat;
 
+import io.netty.channel.group.ChannelGroup;
+import mockit.Deencapsulation;
+import mockit.Mocked;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 public class ChatServerTest {
+  @Mocked
+  ChannelGroup fGroup;
+
+  @Test(expectedExceptions = { NullPointerException.class })
+  public void コンストラクタの引数にnullが指定されたらNullPointerExceptionを投げるよ() {
+    new ChatServer(null);
+  }
+
+  @Test
+  public void コンストラクタの引数が正しく指定されたらちゃんとインスタンスを作るよ() {
+    final ChatServer server = new ChatServer(fGroup);
+
+    assertTrue(Deencapsulation.getField(server, "fGroup") == fGroup);
+    assertNotNull(Deencapsulation.getField(server, "fPanel"));
+    assertTrue(Deencapsulation.getField(server, "fPanel") instanceof ChatView);
+  }
 }
