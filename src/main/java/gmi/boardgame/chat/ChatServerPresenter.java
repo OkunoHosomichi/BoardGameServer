788f97dffcf006187e10ef23f5da0a1ffc69f04a
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-06-24 18:30:53 +0900

    ChatServerPresenterのコンストラクタを実装した。 コンストラクタのテストを作成した。

--- src/main/java/gmi/boardgame/chat/ChatServerPresenter.java
+++ src/main/java/gmi/boardgame/chat/ChatServerPresenter.java
@@ -1,5 +1,24 @@
 package gmi.boardgame.chat;
 
-final class ChatServerPresenter implements ChatPresenter {
+import gmi.utils.exceptions.NullArgumentException;
 
+import javax.inject.Inject;
+
+final class ChatServerPresenter implements ChatPresenter {
+  private final ChatModel fModel;
+
+  /**
+   * 指定されたモデルからインスタンスを構築します。
+   * 
+   * @param model
+   *          モデル。nullを指定できません。
+   * @throws NullPointerException
+   *           view又はmodelがnullの場合。
+   */
+  @Inject
+  public ChatServerPresenter(ChatModel model) throws NullPointerException {
+    if (model == null) throw new NullArgumentException("model");
+
+    fModel = model;
+  }
 }
--- src/test/java/gmi/boardgame/chat/ChatServerPresenterTest.java
+++ src/test/java/gmi/boardgame/chat/ChatServerPresenterTest.java
@@ -1,5 +1,27 @@
 package gmi.boardgame.chat;
 
+import mockit.Deencapsulation;
+import mockit.Mocked;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 public class ChatServerPresenterTest {
+  @Mocked
+  ChatModel fModel;
+  @Mocked
+  ChatView fView;
+
+  @Test(expectedExceptions = { NullPointerException.class })
+  public void コンストラクタの引数にnullが指定されたらNullPointerExceptionを投げるよ() {
+    new ChatServerPresenter(null);
+  }
+
+  @Test
+  public void コンストラクタの引数が正しく指定されたらちゃんとインスタンスを作るよ() {
+    final ChatPresenter presenter = new ChatServerPresenter(fModel);
+
+    assertTrue(Deencapsulation.getField(presenter, "fModel") == fModel);
+  }
 }
