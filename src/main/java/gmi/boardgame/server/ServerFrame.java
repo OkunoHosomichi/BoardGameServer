68003ef640b5453605c62b53f152ec71f2bf0200
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-06-27 19:18:40 +0900

    ServerFrameのコンストラクタを作成した。 コンストラクタのテストを作成した。

--- src/main/java/gmi/boardgame/server/ServerFrame.java
+++ src/main/java/gmi/boardgame/server/ServerFrame.java
@@ -3,5 +3,30 @@ package gmi.boardgame.server;
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
-public class ServerFrame extends JFrame {
+public final class ServerFrame extends JFrame {
+  /**
+   * デフォルトのポート番号。ポート番号は49513～65535までの値を指定します。
+   */
+  private static final int DEFAULT_PORT_NUMBER = 60935;
+  private final int fPortNumber;
+
+  /**
+   * デフォルトのポート番号を指定してインスタンスを構築します。デフォルト値は60935番です。
+   */
+  public ServerFrame() {
+    this(DEFAULT_PORT_NUMBER);
+  }
+
+  /**
+   * ポート番号を指定してインスタンスを構築します。ポート番号は49513～65535までの値を指定します。
+   * 指定された番号が利用可能な範囲外の場合はデフォルトの番号を指定します。
+   * 
+   * @param portNumber
+   *          クライアントの接続を待つポート番号。
+   */
+  public ServerFrame(int portNumber) {
+    assert (DEFAULT_PORT_NUMBER > 49512) && (DEFAULT_PORT_NUMBER < 65536);
+
+    fPortNumber = ((portNumber > 49512) && (portNumber < 65536)) ? portNumber : DEFAULT_PORT_NUMBER;
+  }
 }
---
+++ src/test/java/gmi/boardgame/server/ServerFrameTest.java
@@ -0,0 +1,38 @@
+package gmi.boardgame.server;
+
+import mockit.Deencapsulation;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ServerFrameTest {
+  private static final int DEFAULT_PORT_NUMBER = 60935;
+
+  @Test(groups = { "AllEnv" })
+  public void コンストラクタの引数portNumberに範囲外の値を指定したらデフォルトの番号を設定するよ() {
+    ServerFrame frame = new ServerFrame(49512);
+    assertEquals(Deencapsulation.getField(frame, "fPortNumber"), Integer.valueOf(DEFAULT_PORT_NUMBER));
+
+    frame = new ServerFrame(65536);
+    assertEquals(Deencapsulation.getField(frame, "fPortNumber"), Integer.valueOf(DEFAULT_PORT_NUMBER));
+
+    frame = new ServerFrame(Integer.MIN_VALUE);
+    assertEquals(Deencapsulation.getField(frame, "fPortNumber"), Integer.valueOf(DEFAULT_PORT_NUMBER));
+
+    frame = new ServerFrame(Integer.MAX_VALUE);
+    assertEquals(Deencapsulation.getField(frame, "fPortNumber"), Integer.valueOf(DEFAULT_PORT_NUMBER));
+  }
+
+  @Test(groups = { "AllEnv" })
+  public void コンストラクタの引数を正しく指定したらポート番号に設定するよ() {
+    ServerFrame frame = new ServerFrame(49513);
+    assertEquals(Deencapsulation.getField(frame, "fPortNumber"), Integer.valueOf(49513));
+
+    frame = new ServerFrame(DEFAULT_PORT_NUMBER);
+    assertEquals(Deencapsulation.getField(frame, "fPortNumber"), Integer.valueOf(DEFAULT_PORT_NUMBER));
+
+    frame = new ServerFrame(65535);
+    assertEquals(Deencapsulation.getField(frame, "fPortNumber"), Integer.valueOf(65535));
+  }
+}
