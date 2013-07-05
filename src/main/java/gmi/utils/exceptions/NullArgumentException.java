1d5377b60f40d2db88bcda67100235d2676b22e5
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-07-05 21:21:20 +0900

    NullArgumentExceptionがIllegalArgumentExceptionを継承するようにした。

--- src/main/java/gmi/utils/exceptions/NullArgumentException.java
+++ src/main/java/gmi/utils/exceptions/NullArgumentException.java
@@ -1,25 +1,39 @@
 package gmi.utils.exceptions;
 
-import java.util.Objects;
-
 /**
- * 引数にnullが渡された時にthrowされる例外です。NullPointerExceptionとして扱います。
+ * 引数にnullが渡された時にthrowされる例外です。IllegalArgumentExceptionとして扱います。
  * 例外のメッセージを毎回指定するのが煩わしかったので作りました。
  * 
  * @author おくのほそみち
  */
 @SuppressWarnings("serial")
-public class NullArgumentException extends NullPointerException {
+public class NullArgumentException extends IllegalArgumentException {
   /**
    * 指定された引数名からインスタンスを構築します。 引数名はエラーメッセージに使われます。
    * 
    * @param argName
    *          nullだった引数の名前。nullを指定できません。
-   * @throws NullPointerException
+   * @throws IllegalArgumentException
    *           argNameがnullの場合。
    */
-  public NullArgumentException(String argName) throws NullPointerException {
+  public NullArgumentException(String argName) throws IllegalArgumentException {
 
-    super("引数" + Objects.requireNonNull(argName, "引数argNameにnullを指定できません。") + "にnullを指定できません。");
+    super("引数" + checkArguments(argName) + "にnullを指定できません。");
+  }
+
+  /**
+   * 引数に渡されたオブジェクトがnullかどうかチェックします。nullだった場合はIllegalArgumentExceptionがスローされます。
+   * コンストラクタでsuper() を呼び出す都合でnullチェックをメソッドに分ける必要が出ました。
+   * 
+   * @param arg
+   *          nullチェックを行いたいオブジェクト。
+   * @return 引数のargがそのまま返ります。
+   * @throws IllegalArgumentException
+   *           argがnullの場合。
+   */
+  private static String checkArguments(String arg) throws IllegalArgumentException {
+    if (arg == null) throw new IllegalArgumentException();
+
+    return arg;
   }
 }
