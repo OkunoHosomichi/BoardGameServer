b53a778123af86ef4d31e2d7225c38888fbe70c0
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-07-06 21:27:47 +0900

    テストが通るようにByeCommand#execute()を実装した。

--- src/main/java/gmi/boardgame/chat/commands/ByeCommand.java
+++ src/main/java/gmi/boardgame/chat/commands/ByeCommand.java
@@ -1,6 +1,7 @@
 package gmi.boardgame.chat.commands;
 
 import gmi.utils.chain.Command;
+import gmi.utils.exceptions.NullArgumentException;
 
 /**
  * クライアントの切断処理を行うコマンドです。
@@ -8,10 +9,18 @@ import gmi.utils.chain.Command;
  * @author おくのほそみち
  */
 public class ByeCommand implements Command<ChatCommandContext> {
+  /**
+   * コンテキストのコマンド文字列がこの文字列と等しければMessageコマンドだと判断します。
+   */
+  private static final String COMMAND = "BYE";
 
   @Override
   public boolean execute(ChatCommandContext context) throws IllegalArgumentException {
-    // TODO 自動生成されたメソッド・スタブ
-    return false;
+    if (context == null) throw new NullArgumentException("context");
+    if (!COMMAND.equals(context.getCommand())) return false;
+
+    context.processByeCommand(context.getClient());
+
+    return true;
   }
 }
