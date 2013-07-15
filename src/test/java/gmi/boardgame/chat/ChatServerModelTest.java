df1c7dbac3ce777d4abe0928513f2193fccd2754
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-07-15 11:09:03 +0900

    JMockitの使い方を間違っていた気がするのでChatServerModelのテストを修正した。 まだ修正できる点はあるかもしれないので気付いたら直すようにする。

--- src/test/java/gmi/boardgame/chat/ChatServerModelTest.java
+++ src/test/java/gmi/boardgame/chat/ChatServerModelTest.java
@@ -9,9 +9,6 @@ import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.SocketAddress;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Observer;
@@ -26,38 +23,33 @@ import mockit.NonStrictExpectations;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
 public class ChatServerModelTest {
   private static final String LINE_SEPARATOR = System.lineSeparator();
-  @Mocked
-  private Observer fObserver;
-  @Mocked
-  private ChannelGroup fGroup;
-  @Mocked
-  private Channel fChannel;
 
   @Test(groups = { "AllEnv" })
   public void コンストラクタの引数が正しく指定されたらちゃんとインスタンスを作るよ() {
     final ChatServerModel model = new ChatServerModel();
 
     assertTrue(model.getInformation().isEmpty());
+    assertNotNull(Deencapsulation.getField(model, "fClients"));
   }
 
   @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
   public void updateInformationの引数にnullが指定されたらIllegalArgumentExceptionを投げるよ() {
-    final ChatServerModel model = new ChatServerModel();
-    model.updateInformation(null);
+    new ChatServerModel().updateInformation(null);
   }
 
   @Test(groups = { "AllEnv" })
-  public void updateInformationの引数に空文字列が指定されても何もしないよ() {
+  public void updateInformationの引数に空文字列が指定されても何もしないよ(final Observer observer) {
     final ChatServerModel model = new ChatServerModel();
-    model.addObserver(fObserver);
+    model.addObserver(observer);
 
     new Expectations() {
       {
-        fObserver.update(model, "info");
+        observer.update(model, any);
         times = 0;
       }
     };
@@ -68,13 +60,13 @@ public class ChatServerModelTest {
   }
 
   @Test(groups = { "AllEnv" })
-  public void updateInformationを呼び出されたら通知文を更新してビューに通知するよ() {
+  public void updateInformationを呼び出されたら通知文を更新してビューに通知するよ(final Observer observer) {
     final ChatServerModel model = new ChatServerModel();
-    model.addObserver(fObserver);
+    model.addObserver(observer);
 
     new Expectations() {
       {
-        fObserver.update(model, "info");
+        observer.update(model, "info");
         times = 3;
       }
     };
@@ -88,40 +80,36 @@ public class ChatServerModelTest {
   }
 
   @Test(groups = { "AllEnv" })
-  public void appendInformationの引数に空文字列が指定されたら何もしないよ() throws NoSuchMethodException, SecurityException,
-      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+  public void appendInformationの引数に空文字列が指定されたら何もしないよ(final Observer observer) {
     final ChatServerModel model = new ChatServerModel();
-    model.addObserver(fObserver);
-    final Method appendMessage = ChatServerModel.class.getDeclaredMethod("appendInformation", String.class);
-    appendMessage.setAccessible(true);
+    model.addObserver(observer);
 
     new Expectations() {
       {
-        fObserver.update(model, "info");
+        observer.update(model, any);
         times = 0;
       }
     };
 
-    appendMessage.invoke(model, "");
+    Deencapsulation.invoke(model, "appendInformation", "");
   }
 
   @Test(groups = { "AllEnv" })
-  public void appendInformationを呼び出されたら通知文を更新してビューに通知するよ() throws NoSuchMethodException, SecurityException,
-      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+  public void appendInformationを呼び出されたら通知文を更新してビューに通知するよ(final Observer observer) {
     final ChatServerModel model = new ChatServerModel();
-    model.addObserver(fObserver);
-    final Method appendMessage = ChatServerModel.class.getDeclaredMethod("appendInformation", String.class);
-    appendMessage.setAccessible(true);
+    model.addObserver(observer);
 
     new Expectations() {
       {
-        fObserver.update(model, "info");
+        observer.update(model, "info");
+        times = 2;
       }
     };
 
-    appendMessage.invoke(model, "aaa");
+    Deencapsulation.invoke(model, "appendInformation", "Test");
+    Deencapsulation.invoke(model, "appendInformation", "Information");
 
-    assertEquals(model.getInformation(), "aaa" + LINE_SEPARATOR);
+    assertEquals(model.getInformation(), "Test" + LINE_SEPARATOR + "Information" + LINE_SEPARATOR);
   }
 
   @Test(groups = { "AllEnv" })
@@ -134,16 +122,14 @@ public class ChatServerModelTest {
     final Channel channel2 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
     final Channel channel3 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
     ChannelUtilities.setNickName(channel1, "aaa");
-    ChannelUtilities.setNickName(channel2, "ccc");
-    ChannelUtilities.setNickName(channel3, "eee");
+    ChannelUtilities.setNickName(channel2, "eee");
+    ChannelUtilities.setNickName(channel3, "ccc");
     group.add(channel1);
     group.add(channel2);
     group.add(channel3);
 
     final List<String> result = model.getClientNames();
-    assertEquals(result.size(), 3);
-    assertEquals(result.get(0), "aaa");
-    assertEquals(result.get(2), "eee");
+    assertEquals(result, Arrays.asList("aaa", "ccc", "eee"));
   }
 
   @Test(groups = { "AllEnv" }, expectedExceptions = { UnsupportedOperationException.class })
@@ -176,44 +162,35 @@ public class ChatServerModelTest {
   }
 
   @Test(groups = { "AllEnv" })
-  public void leaveClientを呼ばれたらクライアント一覧を更新してビューに通知するよ() throws NoSuchMethodException, SecurityException,
-      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-    final Method leaveClient = ChatServerModel.class.getDeclaredMethod("leaveClient", new Class[] {});
-    leaveClient.setAccessible(true);
-
+  public void leaveClientを呼ばれたらクライアント一覧を更新してビューに通知するよ(final Observer observer) {
     final ChatServerModel model = new ChatServerModel();
-    model.addObserver(fObserver);
+    model.addObserver(observer);
 
     new Expectations() {
       {
-        fObserver.update(model, "clients");
+        observer.update(model, "clients");
+        times = 1;
       }
     };
 
-    leaveClient.invoke(model, new Object[] {});
+    Deencapsulation.invoke(model, "leaveClient");
   }
 
   @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
   public void sendServerMessageの引数にnullが指定されたらIllegalArgumentExceptionを投げるよ() {
-    final ChatServerModel model = new ChatServerModel();
-    model.sendServerMessage(null);
+    new ChatServerModel().sendServerMessage(null);
   }
 
   @Test(groups = { "AllEnv" })
-  public void sendServerMessageの引数に空文字列が指定されたら何もしないよ() {
+  public void sendServerMessageの引数に空文字列が指定されたら何もしないよ(@Mocked("write") final EmbeddedChannel channel) {
     final ChatServerModel model = new ChatServerModel();
-    Deencapsulation.setField(model, "fClients", fGroup);
+    final ChannelGroup group = Deencapsulation.getField(model, "fClients");
+    group.add(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()));
+    group.add(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()));
 
     new Expectations() {
-      Channel fChannel1;
-      Channel fChannel2;
       {
-        fGroup.iterator();
-        times = 0;
-        result = Arrays.asList(fChannel1, fChannel2).iterator();
-        fChannel1.write("<Server> \n");
-        times = 0;
-        fChannel2.write("<Server> \n");
+        channel.write(any);
         times = 0;
       }
     };
@@ -222,54 +199,47 @@ public class ChatServerModelTest {
   }
 
   @Test(groups = { "AllEnv" })
-  public void sendServerMessageを呼び出されたら各クライアントにサーバからのメッセージを送信するよ() {
+  public void sendServerMessageを呼び出されたら各クライアントにサーバからのメッセージを送信するよ(@Mocked("write") final EmbeddedChannel channel) {
     final ChatServerModel model = new ChatServerModel();
-    Deencapsulation.setField(model, "fClients", fGroup);
+    final ChannelGroup group = Deencapsulation.getField(model, "fClients");
+    group.add(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()));
+    group.add(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()));
+    group.add(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()));
 
     new Expectations() {
-      Channel fChannel1;
-      Channel fChannel2;
       {
-        fGroup.iterator();
-        result = Arrays.asList(fChannel1, fChannel2).iterator();
-
-        fChannel1.write("<Server> これはテストです\n");
-        fChannel2.write("<Server> これはテストです\n");
+        channel.write("<Server> これはテストです。\n");
+        times = 3;
       }
     };
 
-    model.sendServerMessage("これはテストです");
+    model.sendServerMessage("これはテストです。");
   }
 
   @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
   public void processMessageCommandの引数clientにnullが指定されたらIllegalArgumentExceptionを投げるよ() {
-    final ChatServerModel model = new ChatServerModel();
-    model.processMessageCommand(null, "aaa");
+    new ChatServerModel().processMessageCommand(null, "aaa");
   }
 
   @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
-  public void processMessageCommandの引数messageにnullが指定されたらIllegalArgumentExceptionを投げるよ(Channel client) {
+  public void processMessageCommandの引数messageにnullが指定されたらIllegalArgumentExceptionを投げるよ() {
     final ChatServerModel model = new ChatServerModel();
-    model.processMessageCommand(client, null);
+    model.processMessageCommand(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()), null);
   }
 
   @Test(groups = { "AllEnv" })
-  public void processMessageCommandの引数messageに空文字列が指定されたら何もしないよ(final Channel client) {
+  public void processMessageCommandの引数messageに空文字列が指定されたら何もしないよ(@Mocked("write") final EmbeddedChannel channel) {
     final ChatServerModel model = new ChatServerModel();
-    Deencapsulation.setField(model, "fClients", fGroup);
+    final ChannelGroup group = Deencapsulation.getField(model, "fClients");
+    group.add(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()));
+    group.add(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()));
+    group.add(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()));
+    final Channel client = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
+    group.add(client);
 
     new Expectations() {
-      Channel fChannel1;
-      Channel fChannel2;
       {
-        fGroup.iterator();
-        times = 0;
-        result = Arrays.asList(client, fChannel1, fChannel2).iterator();
-        client.write(anyString);
-        times = 0;
-        fChannel1.write(anyString);
-        times = 0;
-        fChannel2.write(anyString);
+        channel.write(any);
         times = 0;
       }
     };
@@ -278,92 +248,113 @@ public class ChatServerModelTest {
   }
 
   @Test(groups = { "AllEnv" })
-  public void processMessageCommandを呼び出されたらコマンドを送信してきたクライアント以外にメッセージを送るよ(final Channel client) {
+  public void processMessageCommandを呼び出されたらコマンドを送信してきたクライアント以外にメッセージを送るよ(@Mocked("write") final EmbeddedChannel channel) {
     final ChatServerModel model = new ChatServerModel();
-    Deencapsulation.setField(model, "fClients", fGroup);
+    final ChannelGroup group = Deencapsulation.getField(model, "fClients");
 
-    new Expectations() {
-      Channel fChannel1;
-      Channel fChannel2;
-      SocketAddress fAddress;
-      {
-        fGroup.iterator();
-        result = Arrays.asList(fChannel1, client, fChannel2).iterator();
-        client.remoteAddress();
-        result = fAddress;
-        fChannel1.write("[" + fAddress.toString() + "] test Message\n");
-        client.write(anyString);
-        times = 0;
-        client.remoteAddress();
-        result = fAddress;
-        fChannel2.write("[" + fAddress.toString() + "] test Message\n");
-      }
-    };
+    final EmbeddedChannel client1 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
+    final EmbeddedChannel client2 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
+    final EmbeddedChannel client3 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
 
-    model.processMessageCommand(client, "test Message");
-  }
+    ChannelUtilities.setNickName(client1, "client1");
+    ChannelUtilities.setNickName(client2, "client2");
+    ChannelUtilities.setNickName(client3, "client3");
 
-  @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
-  public void processByeCommandの引数にnullが指定されたらIllegalArgumentExceptionを投げるよ() {
-    final ChatServerModel model = new ChatServerModel();
-    model.processByeCommand(null);
-  }
-
-  @Test(groups = { "AllEnv" })
-  public void processByeCommandを呼び出されたらクライアントの切断処理を実行するよ(final Channel client) {
-    final ChatServerModel model = new ChatServerModel();
-
-    new Expectations() {
-      {
-        client.close();
-      }
-    };
-
-    model.processByeCommand(client);
-  }
-
-  @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
-  public void processNameCommandの引数clientにnullが指定されたらIllegalArgumentExceptionを投げるよ() {
-    final ChatServerModel model = new ChatServerModel();
-    model.processNameCommand(null, "aaa");
-  }
-
-  @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
-  public void processNameCommandの引数nickNameにnullが指定されたらIllegalArgumentExceptionを投げるよ(Channel client) {
-    final ChatServerModel model = new ChatServerModel();
-    model.processNameCommand(client, null);
-  }
-
-  @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
-  public void processNameCommandの引数nickNameに空文字列が指定されたらIllegalArgumentExceptionを投げるよ(Channel client) {
-    final ChatServerModel model = new ChatServerModel();
-    model.processNameCommand(client, "");
-  }
-
-  @Test(groups = { "AllEnv" })
-  public void processNameCommandを呼び出されたらチャットに参加しているクライアント一覧に登録して他のクライアントに通知するよ(
-      @Mocked("write") final EmbeddedChannel channel) throws Exception {
-    final ChatServerModel model = new ChatServerModel();
-    final ChannelGroup g = Deencapsulation.getField(model, "fClients");
-
-    model.processNameCommand(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()), "test1");
-    model.processNameCommand(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()), "test2");
-    model.processNameCommand(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()), "test3");
+    group.add(client1);
+    group.add(client2);
+    group.add(client3);
 
     new NonStrictExpectations() {
     };
 
-    assertEquals(g.size(), 3);
-    model.processNameCommand(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()), "test");
-    assertEquals(g.size(), 4);
+    model.processMessageCommand(client2, "test Message");
 
     new FullVerifications() {
       {
-        channel.write("ENTER test\n");
-        times = 3;
-        channel.write("Welcome to Chat!\n");
-        channel.write(withPrefix("It is "));
-        channel.write("WELCOME test1,test2,test3\n");
+        onInstance(client1).write("[client2] test Message\n");
+        times = 1;
+        onInstance(client3).write("[client2] test Message\n");
+        times = 1;
+      }
+    };
+  }
+
+  @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
+  public void processByeCommandの引数にnullが指定されたらIllegalArgumentExceptionを投げるよ() {
+    new ChatServerModel().processByeCommand(null);
+  }
+
+  @Test(groups = { "AllEnv" })
+  public void processByeCommandを呼び出されたらクライアントの切断処理を実行するよ(final EmbeddedChannel channel) {
+    final ChatServerModel model = new ChatServerModel();
+
+    new Expectations() {
+      {
+        channel.close();
+      }
+    };
+
+    model.processByeCommand(channel);
+  }
+
+  @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
+  public void processNameCommandの引数clientにnullが指定されたらIllegalArgumentExceptionを投げるよ() {
+    new ChatServerModel().processNameCommand(null, "aaa");
+  }
+
+  @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
+  public void processNameCommandの引数nickNameにnullが指定されたらIllegalArgumentExceptionを投げるよ() {
+    new ChatServerModel().processNameCommand(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()), null);
+  }
+
+  @Test(groups = { "AllEnv" }, expectedExceptions = { IllegalArgumentException.class })
+  public void processNameCommandの引数nickNameに空文字列が指定されたらIllegalArgumentExceptionを投げるよ() {
+    new ChatServerModel().processNameCommand(new EmbeddedChannel(new ChannelOutboundHandlerAdapter()), "");
+  }
+
+  @Test(groups = { "AllEnv" })
+  public void processNameCommandを呼び出されたらチャットに参加しているクライアント一覧に登録してから他のクライアントに通知してビューにも更新を通知するよ(
+      @Mocked final Observer observer, @Mocked("write") final EmbeddedChannel channel) {
+    final ChatServerModel model = new ChatServerModel();
+    model.addObserver(observer);
+    final DefaultChannelGroup group = Deencapsulation.getField(model, "fClients");
+
+    final EmbeddedChannel client1 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
+    final EmbeddedChannel client2 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
+    final EmbeddedChannel client3 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
+    final EmbeddedChannel client4 = new EmbeddedChannel(new ChannelOutboundHandlerAdapter());
+
+    ChannelUtilities.setNickName(client1, "test1");
+    ChannelUtilities.setNickName(client2, "test2");
+    ChannelUtilities.setNickName(client3, "test3");
+
+    group.add(client1);
+    group.add(client2);
+    group.add(client3);
+
+    new NonStrictExpectations() {
+    };
+
+    assertEquals(group.size(), 3);
+    model.processNameCommand(client4, "test4");
+    assertEquals(group.size(), 4);
+
+    new FullVerifications() {
+      {
+        onInstance(client1).write("ENTER test4\n");
+        times = 1;
+        onInstance(client2).write("ENTER test4\n");
+        times = 1;
+        onInstance(client3).write("ENTER test4\n");
+        times = 1;
+        onInstance(client4).write("Welcome to Chat!\n");
+        times = 1;
+        onInstance(client4).write(withMatch("^It is .* now.\\n$"));
+        times = 1;
+        onInstance(client4).write("WELCOME test1,test2,test3\n");
+        times = 1;
+        observer.update(model, "clients");
+        times = 1;
       }
     };
   }
@@ -386,7 +377,7 @@ public class ChatServerModelTest {
 
     new FullVerificationsInOrder() {
       {
-        channel.write("RENAME\n");
+        channel.write("RENAME test2\n");
         times = 1;
       }
     };
@@ -408,7 +399,7 @@ public class ChatServerModelTest {
 
     new FullVerificationsInOrder() {
       {
-        channel.write("RENAME\n");
+        channel.write("RENAME Server\n");
         times = 1;
       }
     };
@@ -422,7 +413,7 @@ public class ChatServerModelTest {
     model.processNameCommand(new EmbeddedChannel(new ChannelInboundHandlerAdapter()), "test");
     model.processNameCommand(new EmbeddedChannel(new ChannelInboundHandlerAdapter()), "test2");
 
-    new Expectations() {
+    new NonStrictExpectations() {
     };
 
     assertEquals(group.size(), 2);
@@ -435,8 +426,9 @@ public class ChatServerModelTest {
 
     new FullVerificationsInOrder() {
       {
-        channel.write("RENAME\n");
-        times = 3;
+        channel.write("RENAME test Name1\n");
+        channel.write("RENAME testN@ame2\n");
+        channel.write("RENAME test,3\n");
       }
     };
   }
