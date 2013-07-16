db7cc812ce443121a870806ec4719ff38627f9c8
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-07-16 19:25:22 +0900

    PreConditions#checkNotEmptyArgument()を追加した。 PreConditions#checkNotNullArgument()を修正した。 テストを追加･修正した。

--- src/main/java/gmi/utils/Preconditions.java
+++ src/main/java/gmi/utils/Preconditions.java
@@ -31,10 +31,26 @@ public final class Preconditions {
    */
   public static final void checkNotNullArgument(@Nullable Object reference, @Nonnull String argName)
       throws IllegalArgumentException {
-    checkArgument(argName != null, getString("NULL_EX0")); //$NON-NLS-1$
-    checkArgument(reference != null, getString("NULL_EX1"), argName); //$NON-NLS-1$
-    //    checkArgument(argName != null, "引数argNameにnullを指定できません。"); //$NON-NLS-1$
-    //    checkArgument(reference != null, "引数%sにnullを指定できません。", argName); //$NON-NLS-1$
+    checkArgument(argName != null, getString("NULL_EX"), "argName"); //$NON-NLS-1$
+    checkArgument(reference != null, getString("NULL_EX"), argName); //$NON-NLS-1$
+  }
+
+  /**
+   * 指定された引数が空文字列かどうかチェックし、空文字列ならIllegalArgumentExceptionをスローします。
+   * 
+   * @param string
+   *          チェックする文字列。nullを指定できません。空文字列が指定されることを考慮してはいますが、
+   *          空文字列だとIllegalArgumentExceptionがスローされます。
+   * @param argName
+   *          チェックする引数の名前。nullを指定できません。
+   * @throws IllegalArgumentException
+   *           string又はargNameがnullの場合。又はstringが空文字列の場合。
+   */
+  public static final void checkNotEmptyArgument(@Nullable String string, @Nonnull String argName)
+      throws IllegalArgumentException {
+    checkArgument(string != null, getString("NULL_EX"), "string"); //$NON-NLS-1$
+    checkArgument(argName != null, getString("NULL_EX"), "argName"); //$NON-NLS-1$
+    checkArgument(!string.isEmpty(), getString("EMPTY_EX"), argName); //$NON-NLS-1$
   }
 
   private static final String getString(String key) {
--- src/test/java/gmi/utils/PreconditionsTest.java
+++ src/test/java/gmi/utils/PreconditionsTest.java
@@ -5,6 +5,9 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 
 public class PreconditionsTest {
+  /*
+   * checkNotNullArgument
+   */
   @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
   public void checkNotNullArgumentの引数argNameがnullならIllegalArgumentExceptionをスローするよ() {
     Preconditions.checkNotNullArgument(new Object(), null);
@@ -18,6 +21,30 @@ public class PreconditionsTest {
   @Test(groups = "AllEnv")
   public void checkNotNullArgumentの引数がどちらもnullでないなら何も起きないよ() {
     Preconditions.checkNotNullArgument(new Object(), "test");
+    assertTrue(true, "何も起きず普通にここまで来るはず。");
+  }
+
+  /*
+   * checkNotEmptyArgument
+   */
+  @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
+  public void checkNotEmptyArgumentの引数stringがnullならIllegalArgumentExceptionをスローするよ() {
+    Preconditions.checkNotEmptyArgument(null, "test");
+  }
+
+  @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
+  public void checkNotEmptyArgumentの引数argNameがnullならIllegalArgumentExceptionをスローするよ() {
+    Preconditions.checkNotEmptyArgument("test", null);
+  }
+
+  @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
+  public void checkNotEmptyArgumentの引数stringが空文字列ならIllegalArgumentExceptionをスローするよ() {
+    Preconditions.checkNotEmptyArgument("", "test");
+  }
+
+  @Test(groups = "AllEnv")
+  public void checkNotEmptyArgumentの引数がどちらもnullでなく引数stringが空文字列でないなら何も起きないよ() {
+    Preconditions.checkNotEmptyArgument("tes", "test");
     assertTrue(true, "何も起きず普通にここまで来るはず。");
   }
 }
