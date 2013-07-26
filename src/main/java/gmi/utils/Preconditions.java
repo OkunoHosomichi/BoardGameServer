9ed78be1122104ddd2f27931b767228fc027bdac
Okuno Hosomichi <okunohosomichi@smoug.net>
2013-07-26 19:48:35 +0900

    Preconditions#checkNotNullOrEmptyArgumentを追加した。 Preconditionsの各メソッドが引数自身を返すように修正した。 テストを追加･修正した。

--- src/main/java/gmi/utils/Preconditions.java
+++ src/main/java/gmi/utils/Preconditions.java
@@ -1,6 +1,5 @@
 package gmi.utils;
 
-import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
@@ -15,42 +14,79 @@ import static com.google.common.base.Preconditions.checkArgument;
  * @author おくのほそみち
  */
 public final class Preconditions {
-  private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(
-      "gmi.utils.PreconditionsMessages", Locale.ROOT); //$NON-NLS-1$
+  private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("gmi.utils.PreconditionsMessages"); //$NON-NLS-1$
 
   /**
-   * 指定された引数がnullかどうかチェックし、nullならIllegalArgumentExceptionをスローします。
-   * 
-   * @param reference
-   *          チェックする引数。
-   *          nullを指定することを考慮してはいますがnullだとIllegalArgumentExceptionがスローされます。
-   * @param argName
-   *          チェックする引数の名前。nullを指定できません。
-   * @throws IllegalArgumentException
-   *           referenceがnullの場合。又はargNameがnullの場合。
+   * インスタンスを構築できないようにprivateにしています。
    */
-  public static final void checkNotNullArgument(@Nullable Object reference, @Nonnull String argName)
-      throws IllegalArgumentException {
-    checkArgument(argName != null, getString("NULL_EX"), "argName"); //$NON-NLS-1$
-    checkArgument(reference != null, getString("NULL_EX"), argName); //$NON-NLS-1$
+  private Preconditions() {
   }
 
   /**
-   * 指定された引数が空文字列かどうかチェックし、空文字列ならIllegalArgumentExceptionをスローします。
+   * 指定された文字列が空文字列かどうかチェックします。空文字列ならIllegalArgumentExceptionをスローし、
+   * 空文字列でなければ文字列自身を返します。
    * 
    * @param string
-   *          チェックする文字列。nullを指定できません。空文字列が指定されることを考慮してはいますが、
-   *          空文字列だとIllegalArgumentExceptionがスローされます。
+   *          チェックする文字列。nullを指定できません。
    * @param argName
    *          チェックする引数の名前。nullを指定できません。
+   * @return string自身。
    * @throws IllegalArgumentException
    *           string又はargNameがnullの場合。又はstringが空文字列の場合。
    */
-  public static final void checkNotEmptyArgument(@Nullable String string, @Nonnull String argName)
-      throws IllegalArgumentException {
+  public static final String checkNotEmptyArgument(@Nonnull String string, @Nonnull String argName) {
     checkArgument(string != null, getString("NULL_EX"), "string"); //$NON-NLS-1$
     checkArgument(argName != null, getString("NULL_EX"), "argName"); //$NON-NLS-1$
-    checkArgument(!string.isEmpty(), getString("EMPTY_EX"), argName); //$NON-NLS-1$
+
+    if (string.isEmpty()) {
+      throw new IllegalArgumentException(getFormattedString("EMPTY_EX", argName));
+    } else {
+      return string;
+    }
+  }
+
+  /**
+   * 指定された引数がnullかどうかチェックします。nullならIllegalArgumentExceptionをスローし、
+   * nullでなければ引数自身を返します。
+   * 
+   * @param reference
+   *          チェックする引数。
+   * @param argName
+   *          チェックする引数の名前。nullを指定できません。
+   * @return reference自身。
+   * @throws IllegalArgumentException
+   *           referenceがnullの場合。又はargNameがnullの場合。
+   */
+  public static final <T> T checkNotNullArgument(@Nullable T reference, @Nonnull String argName) {
+    checkArgument(argName != null, getString("NULL_EX"), "argName"); //$NON-NLS-1$
+
+    if (reference == null) {
+      throw new IllegalArgumentException(getFormattedString("NULL_EX", argName));
+    } else {
+      return reference;
+    }
+  }
+
+  /**
+   * 指定された文字列がnullでも空文字列でもないことをチェックします。null又は空文字列ならIllegalArgumentExceptionをスローし
+   * 、nullでも空文字列でもなければ文字列自身を返します。
+   * 
+   * @param string
+   *          チェックする文字列。
+   * @param argName
+   *          チェックする引数の名前。nullを指定できません。
+   * @return string自身。
+   * @throws IllegalArgumentException
+   *           string又はargNameがnullの場合。又はstringが空文字列の場合。
+   */
+  public static final String checkNotNullOrEmptyArgument(@Nullable String string, @Nonnull String argName) {
+    checkArgument(argName != null, getString("NULL_EX"), "argName"); //$NON-NLS-1$
+
+    return checkNotEmptyArgument(checkNotNullArgument(string, argName), argName);
+  }
+
+  private static final String getFormattedString(String key, String argName) {
+    return String.format(getString(key), argName);
   }
 
   private static final String getString(String key) {
--- src/test/java/gmi/utils/PreconditionsTest.java
+++ src/test/java/gmi/utils/PreconditionsTest.java
@@ -2,9 +2,33 @@ package gmi.utils;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
 
 public class PreconditionsTest {
+  /*
+   * checkNotEmptyArgument
+   */
+  @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
+  public void checkNotEmptyArgumentの引数argNameがnullならIllegalArgumentExceptionをスローするよ() {
+    Preconditions.checkNotEmptyArgument("test", null);
+  }
+
+  @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
+  public void checkNotEmptyArgumentの引数stringがnullならIllegalArgumentExceptionをスローするよ() {
+    Preconditions.checkNotEmptyArgument(null, "test");
+  }
+
+  @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
+  public void checkNotEmptyArgumentの引数stringが空文字列ならIllegalArgumentExceptionをスローするよ() {
+    Preconditions.checkNotEmptyArgument("", "test");
+  }
+
+  @Test(groups = "AllEnv")
+  public void checkNotEmptyArgumentの引数がどちらもnullでなく引数stringが空文字列でないなら引数自身を返すよ() {
+    assertEquals(Preconditions.checkNotEmptyArgument("tes", "test"), "tes");
+  }
+
   /*
    * checkNotNullArgument
    */
@@ -19,32 +43,31 @@ public class PreconditionsTest {
   }
 
   @Test(groups = "AllEnv")
-  public void checkNotNullArgumentの引数がどちらもnullでないなら何も起きないよ() {
-    Preconditions.checkNotNullArgument(new Object(), "test");
-    assertTrue(true, "何も起きず普通にここまで来るはず。");
+  public void checkNotNullArgumentの引数がどちらもnullでないなら引数自身を返すよ() {
+    final Object object = new Object();
+    assertSame(Preconditions.checkNotNullArgument(object, "object"), object);
   }
 
   /*
-   * checkNotEmptyArgument
+   * checkNotNullOrEmptyArgument
    */
   @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
-  public void checkNotEmptyArgumentの引数stringがnullならIllegalArgumentExceptionをスローするよ() {
-    Preconditions.checkNotEmptyArgument(null, "test");
-  }
-
-  @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
-  public void checkNotEmptyArgumentの引数argNameがnullならIllegalArgumentExceptionをスローするよ() {
+  public void checkNotNullOrEmptyArgumentの引数argNameがnullならIllegalArgumentExceptionをスローするよ() {
     Preconditions.checkNotEmptyArgument("test", null);
   }
 
   @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
-  public void checkNotEmptyArgumentの引数stringが空文字列ならIllegalArgumentExceptionをスローするよ() {
+  public void checkNotNullOrEmptyArgumentの引数stringがnullならIllegalArgumentExceptionをスローするよ() {
+    Preconditions.checkNotEmptyArgument(null, "test");
+  }
+
+  @Test(groups = "AllEnv", expectedExceptions = IllegalArgumentException.class)
+  public void checkNotNullOrEmptyArgumentの引数stringが空文字列ならIllegalArgumentExceptionをスローするよ() {
     Preconditions.checkNotEmptyArgument("", "test");
   }
 
   @Test(groups = "AllEnv")
-  public void checkNotEmptyArgumentの引数がどちらもnullでなく引数stringが空文字列でないなら何も起きないよ() {
-    Preconditions.checkNotEmptyArgument("tes", "test");
-    assertTrue(true, "何も起きず普通にここまで来るはず。");
+  public void checkNotNullOrEmptyArgumentの引数がどちらもnullでなく引数stringが空文字列でないなら引数自身を返すよ() {
+    assertEquals(Preconditions.checkNotEmptyArgument("abc", "test"), "abc");
   }
 }
